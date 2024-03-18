bac91fde1ff58fa7e8f1bdd55507b77623c907a2
sjovy <thomas.sjovy@gmail.com>
2024-03-18 20:25:28 +0100

    implemented, fields, setter & getters, and constructor.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -4,6 +4,30 @@ package se.lexicon;
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        // Checking that fields work - they did
+        /*
+        bankaccount customer_1 = new bankaccount();
+        customer_1.setAccount_number(123456789);
+        customer_1.setAccount_balance(50);
+        customer_1.setCustomer_name("Johan Karlsson");
+        customer_1.setCustomer_email("johan@gmail.com");
+        customer_1.setCustomer_phone(7417418);
+        */
+
+        // this works too:
+        bankaccount customer_1 = new bankaccount("Anna Svensson", "anna@yahoo.com", 189115, 987654321, 100);
+        bankaccount customer_2 = new bankaccount("Johan Karlsson", "johan@gmail.com", 7417418, 123456789, 50);
+
+        System.out.println("Customer: " + customer_1.getCustomer_name() + "\n"
+                + "email: " + customer_1.getCustomer_email() + "\n"
+                + "phone: " + customer_1.getCustomer_phone() + "\n"
+                + "Account number: " + customer_1.getAccount_number() + "\n"
+                +  "Balance: " + "$" + customer_1.getAccount_balance());
+
+        System.out.println("Customer: " + customer_2.getCustomer_name() + "\n"
+                + "email: " + customer_2.getCustomer_email() + "\n"
+                + "phone: " + customer_2.getCustomer_phone() + "\n"
+                + "Account number: " + customer_2.getAccount_number() + "\n"
+                +  "Balance: " + "$" + customer_2.getAccount_balance());
     }
 }
